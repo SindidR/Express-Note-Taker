d59ba3ac88bdb7ecc40cf621cf256114e2a1d855
SindidR <sindidreedh@gmail.com>
2022-11-02 17:28:27 -0400

    read-me-finished-and-made-tweaks-to-api-routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,9 +4,9 @@ const path = require('path');
 module.exports = router;
 
     router.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, './notes.html'))
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
   );
   router.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, './index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
   );
 
--- server.js
+++ server.js
@@ -5,10 +5,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 
-
-app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
 app.use('/', htmlRoute);
 app.use('/api', apiRoute);
 
